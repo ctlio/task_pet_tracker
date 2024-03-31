247e0abe264575490b881c74b921212688c1dbbb
crystal tliourane <129141063+ctlio@users.noreply.github.com>
2024-03-31 15:45:46 +0000

    working on navbar and added CDN assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+  root "tasks#home"
+
   devise_for :users
+  
   resources :tasks
   resources :pets
+
+  get ":username" => "users#show", as: :user
+  get ":username/tasks" => "users#tasks", as: :user_tasks
+  get ":username/pets" => "users#pets", as: :user_pet
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-   root "tasks#home"
+   
 end

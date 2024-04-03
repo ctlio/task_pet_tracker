9ed250fb6efa730e9890fe9a338f329d581bd032
crystal tliourane <129141063+ctlio@users.noreply.github.com>
2024-04-03 19:27:20 +0000

    cleaning/refining code

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   get ":username" => "users#show", as: :user
   get ":username/tasks" => "users#tasks", as: :user_tasks
-  #get ":username/tasks_completed" => "users#completed", as: :tasks_completed
+  get ":username/tasks_completed" => "users#completed", as: :tasks_completed
   #get ":username/tasks_failed" => "users#failed", as: :tasks_failed
   get ":username/pets" => "users#pets", as: :user_pets
   
